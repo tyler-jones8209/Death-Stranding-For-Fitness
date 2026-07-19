86d791eb90f2bf23ead83f1ab33574b9b1510ff5
Tyler Jones <tdjones1980@yahoo.com>
2026-07-19 10:49:16 -0500

    Testing and checking

--- test.py
+++ test.py
@@ -16,6 +16,45 @@ class Prepper:
         data = json.loads((folder / "data.json").read_text())
         return clss(**data, folder=folder)
 
-thomas_southerland = Prepper.from_folder(Path("preppers/thomas_southerland"))
 
-print(thomas_southerland.role)
+# identify preppers who are missing a portrait (or is incorrectly named i guess)
+def portrait_check():
+
+    print("MISSING PORTRAITS:\n")
+
+    prepper_folder = Path("preppers/")
+
+    for prepper_path in prepper_folder.iterdir():
+        portrait_path = Path(f"{prepper_path}/face_facility_portrait.jpg")
+
+        if not portrait_path.is_file():
+            print(f"{prepper_path}: Portrait Not Found")
+
+portrait_check()
+
+# identify preppers who are missing a data.json
+def data_check():
+
+    print("MISSING DATA:\n")
+
+    prepper_folder = Path("preppers/")
+
+    for prepper_path in prepper_folder.iterdir():
+        data_path = Path(f"{prepper_path}/data.json")
+
+        if not data_path.is_file():
+            print(f"{prepper_path}: data.json Not Found")
+
+data_check()
+
+
+'''
+prepper_folder = Path("preppers/")
+
+for prepper_path in prepper_folder.iterdir():
+    if not any(Path(prepper_path).iterdir()):
+        continue
+    else:
+        prepper = Prepper.from_folder(prepper_path)
+        print(f"{prepper.name}: {prepper.role}")
+'''
