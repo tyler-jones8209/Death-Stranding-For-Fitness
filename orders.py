7ce403fd5b568c847458f93725bd0e6e3f76c1ad
Tyler Jones <tdjones1980@yahoo.com>
2026-07-22 10:19:52 -0500

    Adding more info/portraits to characters

--- orders.py
+++ orders.py
@@ -1,9 +1,9 @@
 import random
 
-def retrieve_order(prepper1=None, prepper2=None, location1=None, location2=None, misc_location1=None, misc_location2=None):
+def retrieve_order(character1=None, character2=None, location1=None, location2=None, misc_location1=None, misc_location2=None):
 
-    standard = [f"{prepper1} needs you to make a delivery to {location1}.", f"{prepper1} requests that you deliver cargo to {prepper2} at {location1}"]
-    retrieval = [f"{prepper1} lost supplies to a flood. Retrieve them from {misc_location1} and return them to {location1}.", f"An evil group of rental owners stole facility building materials from {prepper1}. Retrieve the stolen supplies from {location1} and return them to {prepper1} at {location2}"]
+    standard = [f"{character1} needs you to make a delivery to {location1}.", f"{character1} requests that you deliver cargo to {character2} at {location1}"]
+    retrieval = [f"{character1} lost supplies to a flood. Retrieve them from {misc_location1} and return them to {location1}.", f"An evil group of rental owners stole facility building materials from {character1}. Retrieve the stolen supplies from {location1} and return them to {character1} at {location2}"]
 
     order = "No Available Order"
 
--- test.py
+++ test.py
@@ -8,66 +8,81 @@ import orders
 
 
 @dataclass
-class Prepper:
+class Character:
     name: str
     game: str
     location: str
+    affiliation: str
     role: str
     image: str
     folder: Path
 
     @classmethod
-    def from_folder(clss, folder: Path) -> "Prepper":
+    def from_folder(clss, folder: Path) -> "Character":
         data = json.loads((folder / "data.json").read_text())
         return clss(**data, folder=folder)
 
 
-# identify preppers who are missing a portrait (or is incorrectly named i guess)
+# identify characters who are missing a portrait (or is incorrectly named i guess)
 def portrait_check():
 
     print("MISSING PORTRAITS:\n")
 
-    prepper_folder = Path("preppers/")
+    character_folder = Path("characters/")
 
-    for prepper_path in prepper_folder.iterdir():
-        portrait_path = Path(f"{prepper_path}/face_facility_portrait.jpg")
+    for character_path in character_folder.iterdir():
+        portrait_path = Path(f"{character_path}/face_facility_portrait.jpg")
 
         if not portrait_path.is_file():
-            print(f"{prepper_path}: Portrait Not Found")
+            print(f"{character_path}: Portrait Not Found")
 
-# identify preppers who are missing a data.json
+# identify characters who are missing a data.json
 def data_check():
 
     print("MISSING DATA:\n")
 
-    prepper_folder = Path("preppers/")
+    character_folder = Path("characters/")
 
-    for prepper_path in prepper_folder.iterdir():
-        data_path = Path(f"{prepper_path}/data.json")
+    for character_path in character_folder.iterdir():
+        data_path = Path(f"{character_path}/data.json")
 
         if not data_path.is_file():
-            print(f"{prepper_path}: data.json Not Found")
+            print(f"{character_path}: data.json Not Found")
 
-def get_avail_preppers():
 
-    avail_prep_paths = []
+# meant to check if the affiliation var was present but this also works if it's present but empty too lmao
+def affiliation_check():
 
-    prepper_folder = Path("preppers/")
+    print("MISSING AFFILIATION:\n")
 
-    for prepper_path in prepper_folder.iterdir():
+    character_folder = Path("characters/")
 
-        data_path = Path(f"{prepper_path}/data.json")
-        portrait_path = Path(f"{prepper_path}/face_facility_portrait.jpg")
+    for character_path in character_folder.iterdir():
+        character = Character.from_folder(character_path)
+
+        if not character.affiliation:
+            print(f"{character_path}: Missing Affiliation")
+
+def get_avail_characters():
+
+    avail_char_paths = []
+
+    character_folder = Path("characters/")
+
+    for character_path in character_folder.iterdir():
+
+        data_path = Path(f"{character_path}/data.json")
+        portrait_path = Path(f"{character_path}/face_facility_portrait.jpg")
 
         if not data_path.is_file() or not portrait_path.is_file():
             continue
 
         else:
-            avail_prep_paths.append(prepper_path)
+            avail_char_paths.append(character_path)
 
-    return avail_prep_paths
+    return avail_char_paths
 
-avail_prep_paths = get_avail_preppers()
+avail_char_paths = get_avail_characters()
 
 random_location1 = business_list[random.randint(0, len(business_list) - 1)]
 random_location2 = business_list[random.randint(0, len(business_list) - 1)]
@@ -79,18 +94,21 @@ while True:
 
 random_misc1 = misc_list[random.randint(0, len(misc_list) - 1)]
 
-random_prepper_path1 = avail_prep_paths[random.randint(0, len(avail_prep_paths) - 1)]
-random_prepper_path2 = avail_prep_paths[random.randint(0, len(avail_prep_paths) - 1)]
+random_character_path1 = avail_char_paths[random.randint(0, len(avail_char_paths) - 1)]
+random_character_path2 = avail_char_paths[random.randint(0, len(avail_char_paths) - 1)]
 
-random_prepper1 = Prepper.from_folder(random_prepper_path1)
-random_prepper2 = Prepper.from_folder(random_prepper_path2)
+random_character1 = Character.from_folder(random_character_path1)
+random_character2 = Character.from_folder(random_character_path2)
 
 while True:
-    if random_prepper_path2 == random_prepper_path1:
-        random_prepper_path2 = avail_prep_paths[random.randint(0, len(avail_prep_paths) - 1)]
-        random_prepper2 = Prepper.from_folder(random_prepper_path2)
+    if random_character_path2 == random_character_path1:
+        random_character_path2 = avail_char_paths[random.randint(0, len(avail_char_paths) - 1)]
+        random_character2 = Character.from_folder(random_character_path2)
     break
 
-random_order = orders.retrieve_order(prepper1=random_prepper1.name, prepper2=random_prepper2.name, location1=random_location1, location2=random_location2, misc_location1=random_misc1)
+random_order = orders.retrieve_order(character1=random_character1.name, character2=random_character2.name, location1=random_location1, location2=random_location2, misc_location1=random_misc1)
 
-print(random_order)
+#print(random_order)
+#portrait_check()
+#data_check()
+#affiliation_check()
